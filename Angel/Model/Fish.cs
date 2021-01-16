b431f9d38381b6f3173deeffab8db6f4d069df16
Tobias Hall <tobbe085@gmail.com>
2021-01-16 08:22:32 +0100

    -Lagt till metod så att det är större risk för att få mindre vikt på fiskar

--- Angel/Model/Fish.cs
+++ Angel/Model/Fish.cs
@@ -26,37 +26,72 @@ namespace Angel
             {
                 FishId = (int)FishEnum.Dace;
                 Species = "Mört";
-                Weight = random.Next(300, 701);
+                //Weight = random.Next(300, 701);
+                Weight = RandomizeWeight(FishId);
                 imagePathFish = new Uri(@"/Resources/Images/Fishes/dace.png", UriKind.Relative);
             }
             else if (randomNumber >= 31 && randomNumber < 41)
             {
                 FishId = (int)FishEnum.Pike;
                 Species = "Gädda";
-                Weight = random.Next(1000, 3001);
+                //Weight = random.Next(1000, 3001);
+                Weight = RandomizeWeight(FishId);
                 imagePathFish = new Uri(@"/Resources/Images/Fishes/pike.png", UriKind.Relative);
             }
             else if (randomNumber >= 41 && randomNumber < 71)
             {
                 FishId = (int)FishEnum.Perch;
                 Species = "Abbore";
-                Weight = random.Next(300, 801);
+                //Weight = random.Next(300, 801);
+                Weight = RandomizeWeight(FishId);
                 imagePathFish = new Uri(@"/Resources/Images/Fishes/perch.png", UriKind.Relative);
             }
             else if (randomNumber >= 71 && randomNumber < 91)
             {
                 FishId = (int)FishEnum.Char;
                 Species = "Röding";
-                Weight = random.Next(500, 1001);
+                //Weight = random.Next(500, 1001);
+                Weight = RandomizeWeight(FishId);
                 imagePathFish = new Uri(@"/Resources/Images/Fishes/char.png", UriKind.Relative);
             }
             else
             {
                 FishId = (int)FishEnum.Trout;
                 Species = "Öring";
-                Weight = random.Next(900, 2000);
+                //Weight = random.Next(900, 2000);
+                Weight = RandomizeWeight(FishId);
                 imagePathFish = new Uri(@"/Resources/Images/Fishes/trout.png", UriKind.Relative);
             }
+        }
+        private int RandomizeWeight(int fishId)
+        {
+            int weightReducer = random.Next(1, 4);
+            int weight;
+            switch (fishId)
+            {
+                case (int)FishEnum.Dace:
+                    weight = random.Next(300, 701);
+                    break;
+                case (int)FishEnum.Pike:
+                    weight = random.Next(1000, 3001);
+                    break;
+                case (int)FishEnum.Perch:
+                    weight = random.Next(300, 801);
+                    break;
+                case (int)FishEnum.Char:
+                    weight = random.Next(500, 1001);
+                    break;
+                case (int)FishEnum.Trout:
+                    weight = random.Next(900, 2000);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            if (weightReducer < 3)
+            {
+                weight = (int)(weight * 0.7);
+            }
+            return weight;
         }
         public override string ToString()
         {
